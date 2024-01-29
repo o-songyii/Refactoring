7710b17f891d0f9e87798229a52687f51590da65
o-songyii <osongyii@gmail.com>
2024-01-29 13:12:15 +0900

    ♻️ 6.15 여러 함수를 클래스로 묶기

--- ch6/6-9-client2.js
+++ ch6/6-9-client2.js
@@ -1,10 +1,6 @@
 import { acquireReading, baseRate } from './6-9.js';
 
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const reading = acquireReading();
 
-function taxThreshold(year) {
-  return 0.1;
-}
-
-export const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+export const taxableCharge = reading.taxableCharge;
+console.log(taxableCharge);
