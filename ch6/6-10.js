0a47f597ed3ff6a1e30d39c11a0a22ab84428cc1
o-songyii <osongyii@gmail.com>
2024-02-12 15:34:37 +0900

    🎨 6.16 여러 함수를 변환함수로 묶기

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,23 @@
+import _ from 'lodash'
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original) {
+  // const result = {...original}; //Object.assign: 얉은 복사
+  const result = _.cloneDeep(original);
+  result.baseCharge = calcuateBaseCharge(result);
+  result.textableCharge = Math.max(
+    0,
+    result.baseCharge - taxTreshold(result.year)
+  );
+  return result;
+}
+
+function calcuateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity
 }
 
 export function baseRate(month, year) {
