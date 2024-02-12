f83f0d66a03d5e0001bea31d7c3f17185b8c2153
o-songyii <osongyii@gmail.com>
2024-02-12 16:09:30 +0900

    🎨 6.21 단계 쪼개기 챌린지2

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,41 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+// 1. run 함수를 만들어서 노드의 process 디펜던시를 제거함
+run(process.argv);
+
+function run (args) {
+  // 2. 사용자에게 입력을 받아옴 -> 유효성 검사
+  const command = parseCommand(args);
+
+  // 3. 필요한 로직 처리
+  countOrders(command);
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  const conutReadyOnly = args.includes('-r');
+
+  return {
+    fileName,
+    conutReadyOnly,
+  }
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders(command) {
+  const rawData = fs.readFileSync(command.fileName);
+  const orders = JSON.parse(rawData);
+  if (command.conutReadyOnly) {
+    console.log(orders.filter((order) => order.status === 'ready').length);
+  } else {
+    console.log(orders.length);
+  }
 }
+
