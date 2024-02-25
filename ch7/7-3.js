4ab672a1c1195f9a5e00ba00d33e0999d824b80d
o-songyii <osongyii@gmail.com>
2024-02-25 16:19:57 +0900

    ⚡️ 7.5 기본형을 객체로 바꾸기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,6 +1,40 @@
+
 export class Order {
   constructor(data) {
     this.priority = data.priority;
+  }
+  
+  //재사용성이 높아짐.
+  isHighPriority() {
+    return this.priority.higherThan('normal');
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      //생성자안에서 에러를 던지는건 보안에 취약함. 좋은 방법은 아님.
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().findIndex(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  static legalValues() {
+    return ['low','normal','high','rush'];
   }
 }
 
@@ -10,6 +44,4 @@ const orders = [
   new Order({ priority: 'rush' }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
